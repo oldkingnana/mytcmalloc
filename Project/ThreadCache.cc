25ee87e37f79b4f5449efca593b044466f147e8e
oldkingnana <2472204250@qq.com>
2026-03-07 22:52:44 +0800

    完善ThreadCache结构,新增了内存大小转换桶pos的算法

--- Project/ThreadCache.cc
+++ Project/ThreadCache.cc
@@ -1,5 +1,84 @@
 #include "ThreadCache.hh"
+#include <cmath>
 
+// ==================FreeList====================
+
+void oldking::FreeList::push(void* pointer)
+{
+	*((void**)pointer) = header_;
+	header_ = pointer;
+}
+
+void* oldking::FreeList::pop()
+{
+	if(header_ == nullptr)
+		return nullptr;
+	void* obj = header_;
+	header_ = *((void**)obj);
+	return obj;
+}
+
+// ==================FreeTable===================
+
+bool oldking::FreeTable::push(void* pointer, uint32_t size)
+{
+	int16_t pos = table_pos(size);
+	if(pos == -1)
+		return false;
+
+	table_[pos].push(pointer);
+	return true;
+}
+
+void* oldking::FreeTable::pop(uint32_t size)
+{
+	return table_[table_pos(size)].pop();
+}
+
+int16_t oldking::FreeTable::table_pos(uint32_t size)
+{
+	int16_t pos = -1;
+
+	if(size == 0)
+		return pos;
+	if(size > TC_MAX_BLOCK)
+		return pos;
+	if(size == 1)
+		return 0;
+
+	// 在不考虑动态对齐长度的情况下
+	
+	//  num                               case                               loss (bytes)                   loss (percent)                 alignment          
+	//   0                          (0, 8] -> 8bytes                              7                           7 / 8 = 87%                       8
+	//   1                          (8, 16] -> 16bytes                            7                           7 / 16 = 43%                      8 
+	//   2                          (16, 24] -> 24bytes                           7                           7 / 24 = 29%                      8 
+	//   3                          (24, 32] -> 32bytes                           7                           7 / 32 = 21%                      8 
+	//   4                          (32, 40] -> 40bytes                           7                           7 / 40 = 17%                      8 
+	//   5                          (40, 48] -> 48bytes                           7                           7 / 48 = 14%                      8 
+	//  ...                               ...                                    ...                              ...                          ...
+	//   X    (alignment * num, alignment * (num + 1)] -> alignment * num    alignment - 1     (alignment - 1) / (alignment * (num + 1))        8
+
+
+	// hardcode (shit)
+
+	uint32_t time = 0;
+	uint32_t begin = 1, end = begin + (TC_BUCKET_NUM * pow(2, time) * TC_BASE_ALIGNMENT);
+	
+	while(size > 96 && begin < TC_MAX_BLOCK)
+	{
+		time++;
+		begin = end;
+		end = begin + (TC_BUCKET_NUM * pow(2, time) * TC_BASE_ALIGNMENT);
+		if(begin <= size && size < end)
+			break;
+	}
+	uint32_t pos_begin = (time) * TC_BUCKET_NUM;
+	pos = pos_begin + ((size - begin == 0) ? 0 : (size - begin) / (pow(2, time) * TC_BASE_ALIGNMENT));
+
+	return pos;
+}
+
+// ==================ThreadCache===================
 
 void* oldking::ThreadCache::New(uint32_t size)
 {
@@ -49,10 +128,8 @@ void oldking::ThreadCache::Delete(void* obj, uint32_t size)
 	// MyEasyLog::GetInstance().WriteLog(LOG_INFO, "ConcurrentMemoryPool", "delete finish");
 }
 
-
-
 uint16_t oldking::ThreadCache::table_pos(uint32_t size)
 {
-	return (TC_BUCKET_NUM * TC_MEMORY_BLOCK_MIN_SIZE) / size;  	
+	(void)size;
+	return 0;
 }
-
--- Project/ThreadCache.hh
+++ Project/ThreadCache.hh
@@ -2,12 +2,62 @@
 
 #include "myeasylog.hpp"
 #include <sys/mman.h>
+// #include <unordered_map>
 
 namespace oldking
 {
-	#define TC_MAX 1024*1024*1024
-	#define TC_BUCKET_NUM 32768
-	#define TC_MEMORY_BLOCK_MIN_SIZE 8
+	const uint32_t TC_MAX = 1024*1024*1024;
+	const uint32_t TC_BUCKET_NUM = 12;  // after every 12 buckets, the alignment will double
+	const uint32_t TC_BASE_ALIGNMENT = 8;
+	const uint32_t TC_MAX_BLOCK = 256*1024;
+
+	//static const std::unordered_map<uint32_t, uint32_t> AlignmentList = 
+	//	{{96, 8}, {288, 16}, {672, 32}, {1440, 64}, {2976, 128}, {6048, 256}, {12192, 512}, {24480, 1024}, {49056, 2048}, {98208, 4096}, {196512, 8192}, {262144, 16384}};
+	// 1~96       -> 8    12
+	// 97~288     -> 16   12
+	// 289~672    -> 32   12 
+	// 673~1440   -> 64   12 
+	// 1441~2976  -> 128  12 
+
+
+	class FreeList
+	{
+	public:
+		FreeList()
+		: header_(nullptr)
+		{}
+
+		~FreeList()
+		{}
+
+		void push(void* pointer);
+
+		void* pop();
+
+	private:
+		void* header_;
+	};
+
+	class FreeTable
+	{
+	public:
+		FreeTable()
+		: table_()
+		{}
+
+		~FreeTable()
+		{}
+
+		bool push(void* pointer, uint32_t size);
+
+		void* pop(uint32_t size);
+
+	//private:
+		static int16_t table_pos(uint32_t size);
+
+	private:
+		std::vector<FreeList> table_;
+	};
 
 	class ThreadCache
 	{
@@ -32,7 +82,6 @@ namespace oldking
 			return free_size_;
 		}
 	
-	private:
 		uint16_t table_pos(uint32_t size);
 
 	private:
